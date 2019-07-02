7fcc4a8ee55de44f8d7cbb061691b801426e44ed
Joshua Crum <joshua.crum@gmail.com>
2019-07-02 00:33:24 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,11 +1,11 @@
 class Puppy
 
   attr_accessor :name
-  attr_reader :breed, :age
+  attr_reader :breed, :months_old
 
   def initialize(params)
     @name = params[:name]
     @breed = params[:breed]
-    @age = params[:age]
+    @age = params[:months_old]
   end
 end
