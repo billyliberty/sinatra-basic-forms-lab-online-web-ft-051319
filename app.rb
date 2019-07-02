bc163c0fa68f3484992af2494af11d20e7152e4b
Joshua Crum <joshua.crum@gmail.com>
2019-07-02 00:36:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,9 +10,8 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/' do
-    @puppy = Puppy.new(params[:name], params[:breed], params[:months_old])
-
+  post '/puppy' do
+    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
     erb :display_puppy
   end
 
